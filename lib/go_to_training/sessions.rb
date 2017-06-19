6f45211ef6ac859a8719502ff769a18943adaed5
Charles Forcey <cforcey@aee.net>
2017-06-19 12:39:11 -0400

    Adjust class namespace of base url constant

--- lib/go_to_training/sessions.rb
+++ lib/go_to_training/sessions.rb
@@ -2,11 +2,11 @@ module GoToTraining
   module Sessions
     
     def get_sessions_by_date_range(params)
-      self.class.post("#{GOTO_TRAINING_BASE_URL}/reports/organizers/#{organizer_key}/sessions", :body => params).parsed_response
+      self.class.post("#{GoToTraining::Client::GOTO_TRAINING_BASE_URL}/reports/organizers/#{organizer_key}/sessions", :body => params).parsed_response
     end
     
     def get_sessions_by_training(training_key)
-      self.class.get("#{GOTO_TRAINING_BASE_URL}/reports/organizers/#{organizer_key}/trainings/#{training_key}").parsed_response
+      self.class.get("#{GoToTraining::Client::GOTO_TRAINING_BASE_URL}/reports/organizers/#{organizer_key}/trainings/#{training_key}").parsed_response
     end
     
   end
